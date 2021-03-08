70668f220f1e9e3278de66224cc523c5218e30c9
Anthony <anthonykmbanks@gmail.com>
2021-03-07 21:13:16 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+const element = document.createElement(`div`);
+document.body.appendChild(element);
+const ul =document.createElement(`ul`);
+
+for (let i = 0; i < 3; i++) {
+    const li = document.createElement('li');
+    li.innerHTML = (i + i).toString();
+    ul.appendChild(li);
+}
+
+main.remove()
+
+const newHeader = document.createElement(`h1`)
+
+newHeader.id = "victory"
+newHeader.innerHTML = 'YOUR-NAME is the champion'
